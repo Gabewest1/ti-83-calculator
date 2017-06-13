f67b4f297f56765432d3747462b0bf0a02274d25
Gabriel West <gabe_west1@yahoo.com>
2017-06-13 17:46:20 -0500

    changed `display: flex` and `flex-wrap: wrap`. Increased height from `5em => 6em`. Removed after and before pseudo selectors and made a `Cursor` and `TextContent` component.

--- src/components/CalculatorScreen.js
+++ src/components/CalculatorScreen.js
@@ -1,27 +1,73 @@
+import React from "react"
 import styled from "styled-components"
 import { Field } from "redux-form"
 
-let CalculatorScreen = styled.div`
+let Screen = styled.div`
+    display:flex;
+    flex-wrap;
     position: relative;
     background-color: gray;
-    height: 5em;
-    width: 100%;
+    height: 6em;
     border-right: 1px solid #1a1a1a;
     border-bottom: 1px solid #1a1a1a;
     border-left: 1px solid #000;
     border-top: 1px solid #000;
     margin-bottom: 1.5em;
-
-    &:after {
-        content: "";
-        background-color: #1a1a1a;
-        position: absolute;
-        left: ${(props) => props.characters * 3 || 3}%;
-        top: 10%;
-        // transform: translateY(-50%);
-        width: .8em;
-        height: 1.3em;
-    }
+    padding: .7em;
+    box-sizing: border-box;
+    word-wrap: break-word;
+`
+let Cursor = styled.span`
+    display: inline-block;
+    background-color: #1a1a1a;
+    width: .7em;
+    height: 1.1em;
+    position: relative;
+    top:3px;
+`
+let TextContent = styled.span`
+    max-width: 100%;
+    word-wrap: break-word;
 `
 
-export default CalculatorScreen
+export default class CalculatorScreen extends React.Component {
+    constructor(props) {
+        super(props)
+        window.addEventListener("resize", this.removeAndSetMaxWidth)
+    }
+    componentDidMount() {
+        console.log("component mounted")
+        this.setScreensMaxWidth()
+    }
+    getScreenDomElement = () => {
+        let screen = document.getElementsByName("calculatorScreen")[0]
+        return screen        
+    }
+    removeAndSetMaxWidth = () => {
+        let screen = this.getScreenDomElement()
+        screen.style.maxWidth = "10000px"
+        console.log("maxWidth: ", screen.style.maxWidth)
+        setTimeout(this.setScreensMaxWidth, 100)
+    }
+    setScreensMaxWidth = () => {
+        let screen = this.getScreenDomElement()        
+        if(!screen) {
+            setTimeout(this.setScreensMaxWidth, 100)
+        } else {
+            let width = window.getComputedStyle(screen, null).getPropertyValue("width")
+            console.log("screen width:", width)
+            screen.style.maxWidth = width
+        }
+    }
+    render() {
+        console.log("Rendering")
+        return (
+            <Screen {...this.props}>
+                <TextContent>
+                    {this.props.content}
+                    <Cursor />
+                </TextContent>
+            </Screen>
+        )
+    }
+}
