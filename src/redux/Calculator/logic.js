2ba6c3cce655331133af9cdd6d6e4bc00138814d
Gabriel West <gabe_west1@yahoo.com>
2017-06-17 22:17:02 -0500

    updated `handleCalculatorButtonClick` logic

--- src/redux/Calculator/logic.js
+++ src/redux/Calculator/logic.js
@@ -2,10 +2,12 @@ import { createLogic } from "redux-logic"
 import types from "./types"
 import actions from "./actions"
 import selectors from "./selectors"
+import {operations as currentLineActions, selectors as currentLineSelectors } from "../CurrentLine"
 import {
     handleEnterButton,
     executeStatement
 } from "./utils"
+
 export const handleCalculatorButtonClick = createLogic({
     type: types.BUTTON_PRESSED,
     transform({getState, action}, next, reject) {
@@ -13,26 +15,37 @@ export const handleCalculatorButtonClick = createLogic({
         button = button.toLowerCase()
         console.log(button)
         switch(button) {
-            case (button.match(/^([0-9])$|^([+-\u00D7\u00F7])$/) || {}).input: {
-                
+            case (button.match(/^([0-9])$|^([+-\u00D7\u00F7])$|^(\(-\))$/) || {}).input: {
                 //Check if the button pressed is the html entity &times; or &divide;
-                //and convert it to its respective multiplication or division sign
+                //and convert it to its respective multiplication or division sign.
+                //If the button is a negative sign which looks like => `(-)` then remove
+                //the parenthesis
                 if(button === "\u00D7") {
                     button = "*"
                 } else if(button === "\u00F7") {
                     button = "/"
+                } else if(button === "(-)") {
+                    button = "-"
                 }
 
-                next(actions.addCharacterToScreen(button))
+                console.log("adding button to screen: ", button)
+                next(currentLineActions.addCharacterToScreen(button))
                 break
             }
             case "clear": {
-                next(actions.clearCalculatorScreen())
+                let shouldClearEntireScreen = currentLineSelectors.selectCursorIndex(getState().currentLine) <= 0
+
+                if(shouldClearEntireScreen) {
+                    next(actions.clearCalculatorScreen())
+                } else {
+                    next(currentLineActions.clearLine())
+                }
+                
                 break
             }
             case "enter": {
-                let state = getState().calculatorScreen
-                let question = selectors.selectQuestion(state)
+                let state = getState().currentLine
+                let question = currentLineSelectors.selectCurrentLine(state)
 
                 try {
                     let answer = executeStatement(question)
@@ -44,6 +57,15 @@ export const handleCalculatorButtonClick = createLogic({
                     break
                 }
 
+            }
+            case "left": {
+                next(currentLineActions.moveCursorBackwards())
+            }
+            case "right": {
+                next(currentLineActions.moveCursorForwards())
+            }
+            case "on": {
+                next(actions.toggleCalculatorPower())
             }
             default:
                 next(action)
@@ -60,7 +82,7 @@ export const createStatement = createLogic({
     process({getState, action}, dispatch, done) {
         let {question, answer} = action
         console.log("Start")
-        dispatch(actions.clearLine())
+        dispatch(currentLineActions.clearLine())
         console.log("After clearLine, before saveQuestion")
         dispatch(actions.saveQuestion(question))
         console.log("After saveQuestion before saveAnswer")
