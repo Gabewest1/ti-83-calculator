d31709f85edb337d97ebfe1e4e4fb7b59441db98
Gabriel West <gabe_west1@yahoo.com>
2017-06-17 22:11:18 -0500

    Removed unused `createNewLine` and `clearLine` action. Added a `toggleCalculatorPower` action

--- src/redux/Calculator/actions.js
+++ src/redux/Calculator/actions.js
@@ -1,23 +1,20 @@
 import types from "./types"
 
 const pressCalculatorButton = (button) => ({ type: types.BUTTON_PRESSED, button })
-const addCharacterToScreen = (character) => ({ type: types.ADD_CHARACTER, character})
 const clearCalculatorScreen = () => ({ type: types.CLEAR_SCREEN })
 const startButtonClickAnimation = (button) => ({ type: types.BUTTON_CLICK_ANIMATION_STARTED, button })
-const createNewLine = () => ({ type: types.CREATE_NEW_LINE })
 const createStatement = (question, answer) => ({ type: types.CREATE_STATEMENT, question, answer })
 const calculateAnswer = (answer) => ({ type: types.CALCULATE_ANSWER })
 const saveAnswer = (answer) => ({ type: types.SAVE_ANSWER, answer })
 const saveQuestion = (question) => ({ type: types.SAVE_QUESTION, question })
-const clearLine = () => ({ type: types.CLEAR_LINE })
+const toggleCalculatorPower = () => ({ type: types.TOGGLE_POWER })
 
 export default {
     pressCalculatorButton,
-    addCharacterToScreen,
     clearCalculatorScreen,
     startButtonClickAnimation,
     createStatement,
     saveAnswer,
     saveQuestion,
-    clearLine,
+    toggleCalculatorPower,
 }
