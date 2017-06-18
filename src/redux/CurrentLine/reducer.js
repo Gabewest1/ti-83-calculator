93781a73ec182b1b122c77173e14b61d6c3605c1
Gabriel West <gabe_west1@yahoo.com>
2017-06-18 00:10:26 -0500

    Updated `CurrentLine` to add spaces before and after operations(+-*/). The `Cursor` skips over blank spaces when moved with the navigation buttons

--- src/redux/CurrentLine/reducer.js
+++ src/redux/CurrentLine/reducer.js
@@ -14,30 +14,52 @@ const currentLineReducer = createReducer(initialCurrentLineState)({
         console.log("Appending new character")
         let { character } = action
         let cursorIndex = state.get("cursorIndex")
+        let increaseCursorIndexAmount = 1
+
+        //if the character is an operation, then add a space before and after it.
+        if(character.search(/[*+-/]/) !== -1) {
+            character = " ".concat(character).concat(" ")
+            increaseCursorIndexAmount += 2
+        }
+        
         return state
                 .update("currentLineText", (currentLineText) => {
                     return currentLineText.slice(0, cursorIndex) + character + currentLineText.slice(cursorIndex)
                 })
-                .update("cursorIndex", (cursorIndex) => cursorIndex+1)
+                .update("cursorIndex", (cursorIndex) => cursorIndex+increaseCursorIndexAmount)
     },
     [types.CLEAR_LINE]: (state, action) => {
         return state.set("currentLineText", "").set("cursorIndex", 0)
     },
     [types.MOVE_CURSOR_BACKWARDS]: (state, action) => {
+        let currentLine = selectors.selectCurrentLine(state)
         let currentIndex = selectors.selectCursorIndex(state)
 
+        //Need to find the next index that isn't a blank space before moving cursor
         if(currentIndex > 0) {
-            return state.update("cursorIndex", (cursorIndex) => currentIndex - 1)
+            let newIndex = currentIndex - 1
+            while(currentLine[newIndex] === " ") {
+                newIndex--
+            }
+
+            return state.set("cursorIndex", newIndex)
         } else {
             return state
         }
     },
     [types.MOVE_CURSOR_FORWARDS]: (state, action) => {
+        let currentLine = selectors.selectCurrentLine(state)        
+        let numOfCharacters = currentLine.length
         let currentIndex = selectors.selectCursorIndex(state)        
-        let numOfCharacters = selectors.selectCurrentLine(state).length
 
+        //Need to find the next index that isn't a blank space before moving cursor        
         if(currentIndex < numOfCharacters) {
-            return state.update("cursorIndex", (cursorIndex) => currentIndex + 1)
+            let newIndex = currentIndex + 1
+            while(currentLine[newIndex] === " ") {
+                newIndex++
+            }
+
+            return state.set("cursorIndex", newIndex)
         } else {
             return state
         }
