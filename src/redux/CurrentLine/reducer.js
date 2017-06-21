41227d6edbd9bab6951df6e9b9fc92444d304b52
Gabriel West <gabe_west1@yahoo.com>
2017-06-20 21:24:11 -0500

    Refactored logic for formatting the text for the current line
    Removed the complex logic for trying to format the current line as input came in, and instead, took all the input at once and then formatted  the current line afterwards.

--- src/redux/CurrentLine/reducer.js
+++ src/redux/CurrentLine/reducer.js
@@ -13,66 +13,22 @@ const currentLineReducer = createReducer(initialCurrentLineState)({
     [types.ADD_CHARACTER]: (state, action) => {
         console.log("Appending new character")
         let { character } = action
-        let cursorIndex = selectors.selectCursorIndex(state)
-        let currentLineText = selectors.selectCurrentLine(state)
-        let characterToBeReplaced = currentLineText.charAt(cursorIndex)        
-        let increaseCursorIndexAmount = 1
+        let cursorIndex = state.get("cursorIndex")
 
-        //if the character is an operation, then add a space before and after it.
-        if(/[*+-/]/.test(character)) {
-            character = " ".concat(character).concat(" ")
-            increaseCursorIndexAmount += 2
-        }
-        
         return state
                 .update("currentLineText", (currentLineText) => {
-
-                    //if replacing an operation, remove the spaces before and after the operation
-                    //and decrease the amount of space the cursor increases by 1
-                    if(/[-+*/]/.test(characterToBeReplaced)) {
-                        increaseCursorIndexAmount--
-                        return currentLineText.slice(0, cursorIndex-1) + character + currentLineText.slice(cursorIndex+2)                        
-                    } else {
-                        return currentLineText.slice(0, cursorIndex) + character + currentLineText.slice(cursorIndex+1)
-                    }
+                    return currentLineText.slice(0, cursorIndex) + character + currentLineText.slice(cursorIndex+1)
                 })
-                .update("cursorIndex", (cursorIndex) => cursorIndex+increaseCursorIndexAmount)
+                .update("cursorIndex", (cursorIndex) => cursorIndex+1)
     },
     [types.CLEAR_LINE]: (state, action) => {
         return state.set("currentLineText", "").set("cursorIndex", 0)
     },
     [types.MOVE_CURSOR_BACKWARDS]: (state, action) => {
-        let currentLine = selectors.selectCurrentLine(state)
-        let currentIndex = selectors.selectCursorIndex(state)
-
-        //Need to find the next index that isn't a blank space before moving cursor
-        if(currentIndex > 0) {
-            let newIndex = currentIndex - 1
-            while(currentLine[newIndex] === " ") {
-                newIndex--
-            }
-
-            return state.set("cursorIndex", newIndex)
-        } else {
-            return state
-        }
+        return state.update("cursorIndex", (index) => index-1)
     },
     [types.MOVE_CURSOR_FORWARDS]: (state, action) => {
-        let currentLine = selectors.selectCurrentLine(state)        
-        let numOfCharacters = currentLine.length
-        let currentIndex = selectors.selectCursorIndex(state)        
-
-        //Need to find the next index that isn't a blank space before moving cursor        
-        if(currentIndex < numOfCharacters) {
-            let newIndex = currentIndex + 1
-            while(currentLine[newIndex] === " ") {
-                newIndex++
-            }
-
-            return state.set("cursorIndex", newIndex)
-        } else {
-            return state
-        }
+        return state.update("cursorIndex", (index) => index+1)
     },
     [types.RESET_PREVIOUS_QUESTION]: (state, action) => {
         let { question } = action
--- src/redux/CurrentLine/selectors.js
+++ src/redux/CurrentLine/selectors.js
@@ -1,7 +1,41 @@
 import { createSelector } from "reselect"
 
-const selectCurrentLine = (state) => state.get("currentLineText")
-const selectCursorIndex = (state) => state.get("cursorIndex")
+const selectCurrentLine = (state) => {
+    let text = state.get("currentLineText")
+    let formattedText = ""
+
+    for(var prop in text) {
+        let char = text[prop]
+        if(/[-+*/]/.test(char)) {
+            formattedText += " "+char+" "
+        } else {
+            formattedText += char
+        }
+    }
+    return formattedText
+}
+
+const selectCursorIndexNumber = (state) => state.get("cursorIndex")
+
+const selectCursorIndex = createSelector(
+    selectCurrentLine,
+    selectCursorIndexNumber,
+    (currentLineText, cursorIndex) => {
+        let currentCharacter = currentLineText.replace(/\s/g, "")[cursorIndex]
+        let numOfOperators = currentLineText
+                                        .replace(/\s/g,"")
+                                        .substring(0, cursorIndex+1)
+                                        .split("")
+                                        .filter(char => /[-+*/]/.test(char))
+                                        .length
+        let numOfSpaces = numOfOperators*2
+        if(/[-+*/]/.test(currentCharacter)) {
+            numOfSpaces--
+        }
+
+        return cursorIndex + numOfSpaces
+    }
+)
 
 export default {
     selectCurrentLine,
